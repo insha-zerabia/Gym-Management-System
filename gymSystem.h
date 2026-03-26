13bc96d19eb6f0db16ec5b80b7a31e71c6b617f5
Insha Zerabia <l252042@lhr.nu.edu.pk>
2026-03-26 20:10:50 +0500

    Add GymSystem class interface and update header guards
    Added includes for related modules and defined the GymSystem class with methods for data management, menus, and login functionality.

--- gymSystem.h
+++ gymSystem.h
@@ -1,1 +1,38 @@
-#pragma once
+#ifndef GYMSYSTEM_H
+#define GYMSYSTEM_H
+
+#include "member.h"
+#include "trainer.h"
+#include "plan.h"
+#include "payment.h"
+#include "workout.h"
+#include "attendance.h"
+
+class GymSystem {
+public:
+    GymSystem();
+
+    void loadAllData();
+    void saveAllData();
+
+    void run();               
+    void showStartMenu();                                    //Admin/User
+    void adminMenu();
+    void memberMenu();
+
+    //part of module 1,2,3
+    void handleMemberManagement();
+    void handleTrainerManagement();
+    void handlePlanAndPayment();
+    void handleWorkoutManagement();
+    void handleAttendance();
+
+    // login
+    bool adminLogin();
+    bool memberLogin();
+
+private:
+
+};
+
+#endif
