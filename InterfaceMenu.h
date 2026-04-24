c72dfb393d1c8712f5bbe10d64e966b2a611d7f5
Insha Zerabia <l252042@lhr.nu.edu.pk>
2026-04-24 22:37:19 +0500

    Add admin menu module and interface base class
    Added adminMenu.cpp, adminMenu.h, and InterfaceMenu.h for admin panel functionality. Updated project files to include new sources. Introduced InterfaceMenu as an abstract base class for menu interfaces. Implemented AdminMenu with menu display, input handling, and placeholders for management modules.

--- InterfaceMenu.h
+++ InterfaceMenu.h
@@ -5,12 +5,12 @@
 //This is an abstract class for Interface
 //AdminMenu and UserMenu will inherit from this
 
-class IMenu {
+class InterfaceMenu {
 public:
 
     virtual void showMenu() = 0;
     virtual void handleInput() = 0;
-    virtual ~IMenu() {}
+    virtual ~InterfaceMenu() {}
 };
 
 #endif
