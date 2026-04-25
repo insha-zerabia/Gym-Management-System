c182d8f2b0f50505fb7aad3d151fd41d9733ecba
zemal-irfan <zemalirfan0728@gmail.com>
2026-04-25 02:54:57 -0700

    Added third file of module 3

--- payment.h
+++ payment.h
@@ -1,1 +1,29 @@
-#pragma once
+#ifndef payment_h
+#define payment_h
+#include <string>
+using namespace std;
+class Payment{
+int paymentID;
+int memberID;
+string date;
+double amount;
+bool isPaid;
+public:
+Payment();
+void recordOfPayment(int memberID, double amount);
+int getMemberID() const;
+double getAmount() const;
+bool getStatus() const;
+};
+class PaymentManagement{
+Payment Payments[100];
+int count;
+public:
+void addPayment(Payment p);
+void showPaymentHistory(int memberID);
+void checkUnpaidMembers();
+void createReport();
+void loadPayments();
+void savePayments();
+};
+#endif
