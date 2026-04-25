8d12e28656296b4cb8967634eda4b1411107ea45
l252051-blip <l252051@lhr.nu.edu.pk>
2026-04-24 23:06:56 -0700

    Added one file of module 3

--- plan.h
+++ plan.h
@@ -1,1 +1,34 @@
-#pragma once
+#ifndef plan_h
+#define plan_h
+#include<string>
+using namespace std;
+class Plan{
+int ID;
+string name;
+int period;
+double fee;
+public:
+Plan();
+Plan(int ID,string name,int period, double fee);
+void setID(int ID);
+void setName(string name);
+void setPeriod(int period);
+void setFee(double fee);
+int getID() const;
+string getName() const;
+int getPeriod() const;
+double getFee() const;
+
+};
+class PlanManagement{
+    Plan plans[100];
+    int count;
+    public:
+    PlanManagement();
+    void addPlan(Plan p);
+    void viewPlans();
+    void updatePlan(int ID);
+    void loadPlans();
+    void savePlans();
+};
+#endif
