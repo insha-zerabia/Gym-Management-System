36c49008ef00375b097af8fdd9d6dbd22d6a1f6c
zemal-irfan <zemalirfan0728@gmail.com>
2026-04-28 05:42:01 -0700

    Used functions in main file

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@ using namespace std;
 int main() {
     GymSystem gym;
     gym.run();
+    PlanManagement pm;
+pm.loadPlans();
+pm.addPlan(Plan(1, "Basic", 30, 1000));
+pm.viewPlans();
+pm.savePlans();
+PaymentManagement pay;
+
+Payment p;
+p.recordOfPayment(101, 1000);
+
+pay.addPayment(p);
+pay.showPaymentHistory(101);
+pay.createReport();
     return 0;
 }
