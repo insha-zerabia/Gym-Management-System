b28a5927ae5ad6f685176d9f0626ce17439abac3
zemal-irfan <zemalirfan0728@gmail.com>
2026-04-28 00:49:44 -0700

    Added features in plan file

--- plan.cpp
+++ plan.cpp
@@ -1,7 +1,13 @@
 #include "plan.h"
 #include <iostream>
+#include<fstream>
 using namespace std;
-Plan::Plan() {}
+Plan::Plan() {
+    ID=0;
+    name="";
+    period=0;
+    fee=0;
+}
 Plan::Plan(int ID, string name, int period, double fee)
 {
     this->ID=ID;
@@ -44,21 +50,64 @@ PlanManagement::PlanManagement()
 }
 void PlanManagement::addPlan(Plan p)
 {
-
+if (count<100)
+{
+    plans[count++]=p;
+}
+else{
+    cout<<"list is full"<<endl;
+}
 }
 void PlanManagement::viewPlans()
 {
-
+for(int i=0;i<count;i++)
+{
+    cout<<" ID: "<<plans[i].getID()<<endl<<"Name:"<<plans[i].getName()<<endl<<"Period: "<<plans[i].getPeriod()<<endl<<"Fee: "<<plans[i].getFee()<<endl;
+}
 }
 void PlanManagement::updatePlan(int ID)
 {
-
+for(int i=0;i<count;i++)
+{
+if(plans[i].getID() ==ID)
+{
+    string name;
+    int period;
+    double fee;
+    cout<<"Enter new name: ";
+    cin>>name;
+    cout<<"Enter period: ";
+    cin>>period;
+    cout<<"Enter fee: ";
+    cin>>fee;
+    plans[i].setName(name);
+    plans[i].setPeriod(period);
+    plans[i].setFee(fee);
+    cout<<"Plan is updated."<<endl;
+    return;
+}
+}
+cout<<"Plan is not found. "<<endl;
 }
 void PlanManagement::loadPlans()
 {
-
+ifstream file("plans.txt");
+count=0;
+int ID, period;
+string name;
+double fee;
+while(file>> ID >>name>>period>>fee)
+{
+plans[count++]=Plan(ID,name,period,fee);
+}
+file.close();
 }
 void PlanManagement::savePlans()
 {
-    
+ofstream file("plans.txt");
+for(int i=0;i<count;i++)
+{
+    file<<plans[i].getID()<<" "<<plans[i].getName()<<" "<<plans[i].getPeriod()<<" "<<plans[i].getFee()<<endl;
+}
+file.close();
 }
